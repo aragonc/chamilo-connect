f34a99d1cf224c0f7f22d61e21caac8255277c03
Alex Aragon <aragcar@gmail.com>
2023-06-26 18:24:54 -0500

    add register user in wordpress

--- chamilo-connect.php
+++ chamilo-connect.php
@@ -68,7 +68,7 @@ function chamilo_create_menu()
 
 //add css boostrap
 // Incluir Bootstrap CSS
-function bootstrap_css() {
+function resource_css() {
     $urlPlugin = plugin_dir_url( __FILE__ );
     wp_enqueue_style( 'bootstrap_css',
         'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
@@ -80,23 +80,37 @@ function bootstrap_css() {
         array(),
         '1.0'
     );
+    wp_enqueue_style( 'strength_css',
+        $urlPlugin.'js/strength/strength.css',
+        array(),
+        '1.0'
+    );
 }
-add_action( 'wp_enqueue_scripts', 'bootstrap_css');
+add_action( 'wp_enqueue_scripts', 'resource_css');
 
 // Incluir Bootstrap JS y dependencia popper
-function bootstrap_js() {
+function resource_js() {
+    $urlPlugin = plugin_dir_url( __FILE__ );
     wp_enqueue_script( 'popper_js',
         'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
         array(),
         '1.14.3',
-        true);
+        true
+    );
     wp_enqueue_script( 'bootstrap_js',
         'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',
         array('jquery','popper_js'),
         '4.1.3',
-        true);
+        true
+    );
+    wp_enqueue_script('strength_js',
+        $urlPlugin.'js/strength/strength.min.js',
+        array(),
+        '1.0',
+        true
+    );
 }
-add_action( 'wp_enqueue_scripts', 'bootstrap_js');
+add_action( 'wp_enqueue_scripts', 'resource_js');
 
 function chamilo_create_pages_user()
 {
--- templates/tpl_user_register.php
+++ templates/tpl_user_register.php
@@ -4,8 +4,38 @@
  */
 
 get_header();
+
+if(is_user_logged_in()){
+    header( home_url()."/user-account");
+} else {
 $urlLogin = home_url().'/user-login';
 $urlLostPassword = home_url().'/user-lostpassword';
+include (plugin_dir_path( __FILE__ ) .'../countries/countries.php');
+$countries = getCountries();
+
+// post form
+
+    if (isset($_POST['register-submit'])) {
+        $params =  [
+            'first_name' => $_POST['firstname'],
+            'last_name' => $_POST['lastname'],
+            'user_email' => $_POST['email'],
+            'user_login' => $_POST['email'],
+            'user_pass' => $_POST['password'],
+            'display_name' => $_POST['firstname'].', '.$_POST['lastname']
+        ];
+
+        $user = wp_insert_user($params);
+
+        if (is_wp_error($user)) {
+            $error_message = $user->get_error_message();
+        } else {
+            add_user_meta($user,'country',$_POST['country']);
+            add_user_meta($user,'identifier',$_POST['identifier']);
+            add_user_meta($user,'rut',$_POST['rut']);
+            header( home_url()."/user-login");
+        }
+    }
 
 ?>
     <div class="container">
@@ -17,6 +47,9 @@ $urlLostPassword = home_url().'/user-lostpassword';
 
                 </div>
                 <div class="col-md-7">
+                    <div id="msg-error-rut" style="display: none;" class="alert alert-danger">
+                        Debe de ingresar un RUT Válido
+                    </div>
                     <form method="post" action="" id="register-user" class="register-user">
                         <div class="row">
                             <div class="col-md-12">
@@ -29,13 +62,13 @@ $urlLostPassword = home_url().'/user-lostpassword';
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="firstname">Nombres (*)</label>
-                                    <input type="text" class="form-control" id="firstname">
+                                    <input type="text" class="form-control" name="firstname" id="firstname" required>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="lastname">Apellidos (*)</label>
-                                    <input type="text" class="form-control" id="lastname">
+                                    <input type="text" class="form-control" name="lastname" id="lastname" required>
                                 </div>
                             </div>
                         </div>
@@ -43,7 +76,7 @@ $urlLostPassword = home_url().'/user-lostpassword';
                             <div class="col-md-12">
                                 <div class="form-group">
                                     <label for="email">Correo electrónico (*)</label>
-                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" required>
                                     <small id="emailHelp" class="form-text text-muted">
                                         Este será tu usuario de acceso para ingresar a nuestra aula virtual, solo se aceptan minúsculas
                                     </small>
@@ -51,19 +84,14 @@ $urlLostPassword = home_url().'/user-lostpassword';
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group">
-                                    <label for="password1">Contraseña</label>
-                                    <input type="password" class="form-control" id="password1" aria-describedby="passwordHelp">
+                                    <label for="password">Contraseña</label>
+                                    <input type="password" class="form-control" id="password" name="password" aria-describedby="passwordHelp" required>
+                                    <div id="paswordtrength"></div>
                                     <small id="passwordHelp" class="form-text text-muted">
                                         Establece la contraseña que utilizarás para acceder a nuestra aula virtual, usar entre mayúsculas,minúsculas,caracteres especiales, sin espacios
                                     </small>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="password2">Confirme contraseña</label>
-                                    <input type="password" class="form-control" id="password2">
                                 </div>
                             </div>
                         </div>
@@ -71,19 +99,32 @@ $urlLostPassword = home_url().'/user-lostpassword';
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="country">Pais</label>
-                                    <select name="country" class="form-control" id="country">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
+                                    <select name="country" class="form-control" id="country" required>
+                                        <?php foreach ($countries as $country):
+                                            $selected = '';
+                                            if(strtoupper($country['alpha2']) == 'CL') {
+                                                $selected = 'selected';
+                                            }
+                                        ?>
+                                        <option value="<?php echo strtoupper($country['alpha2']); ?>" <?php echo $selected; ?>><?php echo $country['name']; ?></option>
+                                        <?php endforeach; ?>
                                     </select>
                                 </div>
                             </div>
                             <div class="col-md-6">
-                                <div class="form-group">
+                                <div id="input_dni" class="form-group" style="display:none;">
                                     <label for="identifier">Nº Documento o Cédula de Identidad (*)</label>
-                                    <input type="text" class="form-control" id="identifier">
+                                    <input type="text" name="identifier" class="form-control" id="identifier">
+                                    <small id="identifier_help" class="form-text text-muted">
+                                        Escribe tu DNI o Documento de identidad
+                                    </small>
+                                </div>
+                                <div id="input_rut" class="form-group">
+                                    <label for="rut">RUT Identificador Nacional (*)</label>
+                                    <input type="text" name="rut" class="form-control" id="rut" placeholder="Ej: 11222333-K">
+                                    <small id="rut_hep" class="form-text text-muted">
+                                        Ingresar RUN sin puntos, con guión y con dígito verificador. Ej: 11222333-K
+                                    </small>
                                 </div>
                             </div>
                         </div>
@@ -91,12 +132,83 @@ $urlLostPassword = home_url().'/user-lostpassword';
                             * Esta información la utilizaremos para tu certificado de aprobación.
                         </div>
                         <div class="form-group">
-                            <button type="submit" value="submit" class="btn btn-primary btn-block">Registrarme</button>
+                            <button type="submit" id="register-submit" name="register-submit" value="register-submit" class="btn btn-primary btn-block">Registrarme</button>
                         </div>
                     </form>
                 </div>
             </div>
         </section>
     </div>
+    <script>
+        (function($){
+            let rut = $("#rut");
+            let dni = $("#dni");
+            let checkRut = true;
 
-<?php get_footer(); ?>
+            $('#password').keyup(function(e) {
+                let html = '';
+                let strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+                let mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+                let enoughRegex = new RegExp("(?=.{6,}).*", "g");
+                if (false == enoughRegex.test($(this).val())) {
+                    html = '<div class="alert alert-primary" role="alert">Ingresa más de 6 caracteres</div>';
+                    $('#paswordtrength').html(html);
+                } else if (strongRegex.test($(this).val())) {
+                    html = '<div class="alert alert-success" role="alert">Contraseña fuerte!</div>';
+                    $('#paswordtrength').html(html);
+                } else if (mediumRegex.test($(this).val())) {
+                    html = '<div class="alert alert-warning" role="alert">Contraseña media!</div>';
+                    $('#paswordtrength').html(html);
+                } else {
+                    html = '<div class="alert alert-danger" role="alert">Contraseña débil!</div>';
+                    $('#paswordtrength').html(html);
+                }
+                return true;
+            });
+
+            $("#country").change(function () {
+                let countrySelect;
+                rut.attr('title','Ingresar RUN sin puntos, con guión y con dígito verificador. Ej: 11222333-K');
+                rut.attr('maxlength','10');
+                $( "#country option:selected" ).each(function() {
+                    countrySelect = $(this).val();
+                    //console.log(countrySelect);
+                    if(countrySelect == 'CL'){
+                        $("#input_dni").hide();
+                        $("#input_rut").show();
+                        rut.val('');
+                    } else {
+                        $("#input_dni").show();
+                        $("#input_rut").hide();
+                        dni.val('');
+                    }
+                });
+            });
+
+            $("#register-user").submit(function(e){
+                //console.log(RUT.val());
+                let RutValue = rut.val();
+                let countrySelect;
+                //alert($("input[type=radio]:checked").val());
+                $( "#country option:selected" ).each(function() {
+                    countrySelect = $(this).val();
+                });
+                console.log(countrySelect);
+                if(checkRut){
+                    if(countrySelect === 'CL') {
+                        if (!(RutValue.match('^[0-9]{7,9}[-|‐]{1}[0-9kK]{1}$'))) {
+                            $("#msg-error-rut").show();
+                            e.preventDefault();
+                        }
+                    }
+                }
+            });
+
+        })(jQuery);
+
+    </script>
+<?php
+}
+    get_footer();
+
+?>
